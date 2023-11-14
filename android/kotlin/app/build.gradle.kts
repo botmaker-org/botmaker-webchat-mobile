860e6339d8fc5df2d55e0874e9b4e4d3f516f413
dariopepper <dario.pepper@botmaker.io>
2023-11-14 16:37:29 -0300

    Fixing config gradle kotlin

--- android/kotlin/app/build.gradle.kts
+++ android/kotlin/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    kotlin("android")
 }
 
 android {
@@ -29,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
@@ -38,4 +42,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
 }
